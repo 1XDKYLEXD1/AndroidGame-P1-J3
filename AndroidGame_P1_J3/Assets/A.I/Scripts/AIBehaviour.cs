31c5a5ad52822f5a97b9cf3918d1902370825e92
1XDKYLEXD1 <520163@student.glu.nl>
2019-09-24 19:56:42 +0200

    Added some more folders and placeholders to the project + Added the AI Distance check

---
+++ AndroidGame_P1_J3/Assets/A.I/Scripts/AIBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIFightBehaviourState
+{
+    Nearby,
+    MediumRange,
+    FarAway,
+    Anywhere
+};
+
+public enum EntityAnimationState
+{
+    Idle,
+    Walking,
+    Running
+};
+
+public enum AIFightingMoves
+{
+    Slice,
+    Stab,
+    Block,
+    DashTowards,
+    DashAway,
+    Jump,
+    JumpAttack,
+};
+
+public class AIBehaviour : MonoBehaviour
+{
+    [SerializeField] Transform m_player;
+    Rigidbody2D m_myrgigibody;
+
+    AIFightBehaviourState m_fightbehaviour;
+    AIFightingMoves m_fightingmoves;
+    EntityAnimationState m_animationstate;
+
+    void Awake()
+    {
+        m_myrgigibody = GetComponent<Rigidbody2D>();
+        m_animationstate = EntityAnimationState.Idle;
+    }
+
+    void Update()
+    {
+        Debug.Log("Distance between P and AI: " + DistanceCheck());
+    }
+
+    float DistanceCheck()
+    {
+        float distance = Vector2.Distance(transform.position, m_player.transform.position);
+        return distance;
+    }
+}
