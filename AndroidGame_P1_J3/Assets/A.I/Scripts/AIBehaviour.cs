c6538e190013f310b45980c68cac92f7ad5399c8
1XDKYLEXD1 <520163@student.glu.nl>
2019-09-25 20:05:18 +0200

    Added the AI fight behaviour calculations

--- AndroidGame_P1_J3/Assets/A.I/Scripts/AIBehaviour.cs
+++ AndroidGame_P1_J3/Assets/A.I/Scripts/AIBehaviour.cs
@@ -33,6 +33,15 @@ public class AIBehaviour : MonoBehaviour
     [SerializeField] Transform m_player;
     Rigidbody2D m_myrgigibody;
 
+    //Variables to calculate what moveset to use;
+    [SerializeField] float m_nearbynode;
+    [SerializeField] float m_mediumnode;
+    [SerializeField] float m_farnode;
+    [SerializeField] float m_anywherenode;
+
+    [SerializeField] float m_fastestmovespeed;
+    [SerializeField] float m_slowestmovespeed;
+
     AIFightBehaviourState m_fightbehaviour;
     AIFightingMoves m_fightingmoves;
     EntityAnimationState m_animationstate;
@@ -45,12 +54,93 @@ public class AIBehaviour : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("Distance between P and AI: " + DistanceCheck());
+        m_fightbehaviour = CalculateFightBehaviour(m_nearbynode, m_mediumnode, m_farnode, m_anywherenode);
+
+        switch(m_fightbehaviour)
+        {
+            case AIFightBehaviourState.Nearby:
+                //RunAwayFromTarget();
+                break;
+
+            case AIFightBehaviourState.MediumRange:
+                //WalkAwayFromTarget();
+                break;
+
+            case AIFightBehaviourState.FarAway:
+                RunTowardsTarget();
+                break;
+
+            case AIFightBehaviourState.Anywhere:
+                break;
+        }
+
+        Debug.Log("Distance: " + DistanceCheck(m_player));
+        Debug.Log("AIFightBehaviour: " + m_fightbehaviour);
     }
 
-    float DistanceCheck()
+    float DistanceCheck(Transform target)
     {
-        float distance = Vector2.Distance(transform.position, m_player.transform.position);
+        float distance = Vector2.Distance(transform.position, target.transform.position);
+
         return distance;
+    }
+
+    void RunTowardsTarget()
+    {
+        m_myrgigibody.velocity = new Vector2(m_fastestmovespeed, 0);
+    }
+    void WalkTowardsTarget()
+    {
+        m_myrgigibody.velocity = new Vector2(m_slowestmovespeed, 0);
+    }
+
+    void RunAwayFromTarget()
+    {
+        m_myrgigibody.velocity = new Vector2(-m_fastestmovespeed, 0);
+    }
+    void WalkAwayFromTarget()
+    {
+        m_myrgigibody.velocity = new Vector2(-m_slowestmovespeed, 0);
+    }
+    
+
+    AIFightBehaviourState CalculateFightBehaviour(float nearnode, float mednode, float farnode, float anywnode)
+    {
+        float neardistance = nearnode - DistanceCheck(m_player);
+        float meddistance = mednode - DistanceCheck(m_player);
+        float fardistance = farnode - DistanceCheck(m_player);
+        float anywdistance = anywnode - DistanceCheck(m_player);
+
+        if(neardistance < 0) { neardistance = neardistance * (-1); }
+        if (meddistance < 0) { meddistance = meddistance * (-1); }
+        if (fardistance < 0) { fardistance = fardistance * (-1); }
+        if (anywdistance < 0) { anywdistance = anywdistance * (-1); }
+
+        Mathf.Abs(neardistance);
+        Mathf.Abs(meddistance);
+        Mathf.Abs(fardistance);
+        Mathf.Abs(anywdistance);
+
+        if (neardistance < meddistance && neardistance < fardistance && neardistance < anywdistance)
+        {
+            return AIFightBehaviourState.Nearby;
+        }
+        else if (meddistance < neardistance && meddistance < fardistance && meddistance < anywdistance)
+        {
+            return AIFightBehaviourState.MediumRange;
+        }
+        else if (fardistance < neardistance && fardistance < meddistance && fardistance < anywdistance)
+        {
+            return AIFightBehaviourState.FarAway;
+        }
+        else if (anywdistance < neardistance && anywdistance < meddistance && anywdistance < fardistance)
+        {
+            return AIFightBehaviourState.Anywhere;
+        }
+        else
+        {
+            Debug.LogError("Something is wrong in the AI calculations or every number is the same");
+            return AIFightBehaviourState.Anywhere;
+        }
     }
 }
